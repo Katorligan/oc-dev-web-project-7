654dcca98fa1da603f1b03fa258fc8a73c2781ba
Katorligan <katorligan@gmail.com>
2023-07-12 11:53:31 +0200

    refactor: move mongodb connection to app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const booksRoutes = require('./routes/books');
 
 const app = express();
 
 app.use(express.json());
+
+mongoose
+	.connect('mongodb+srv://katorligan:4pQjcENGqINE0qWu@cluster0.grinbdt.mongodb.net/?retryWrites=true&w=majority', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log('Connected to MongoDB.'))
+	.catch(() => console.log('Connection to MongoDB failed.'));
 
 app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
